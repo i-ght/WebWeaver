7f03fa899ced47cb1ef30100dabbbf7b0aabf14c
Circuitous Silence <toph.ght@gmail.com>
2024-12-19 07:30:03 -0600

    Dream weaver

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::{BTreeMap, HashSet};
 use std::error::Error;
+use std::ffi::OsString;
 use std::fs::{self, DirEntry};
 use std::path::{Path, PathBuf};
 use std::{env, io};
@@ -13,7 +14,14 @@ https://www.tutorialspoint.com/rss/rss2.0-tag-syntax.htm
 https://www.ietf.org/rfc/rfc4287.txt
 */
 
-fn argv() -> io::Result<Vec<String>> {
+struct Argv {
+    /* Folder to read content files from */
+    input_content_path: PathBuf,
+    /* Output will be stored in ./content/output_content_root_path if it's not None. */
+    output_content_path: PathBuf,
+}
+
+fn argv() -> io::Result<Argv> {
     let argv: Vec<String> = env::args().collect();
     if argv.len() <= 1 {
         return Err(io::Error::new(
@@ -21,6 +29,31 @@ fn argv() -> io::Result<Vec<String>> {
             "specify content path in first argument.",
         ));
     }
+
+    let input_content_path = PathBuf::from(&argv[1]);
+    let output_content_root_path = PathBuf::from(&argv[2].trim_end_matches('/'));
+
+    let argv = Argv {
+        input_content_path,
+        output_content_path: output_content_root_path,
+    };
+
+    let input_exists = argv.input_content_path.exists();
+    let input_is_dir = argv.input_content_path.is_dir();
+
+    if !input_exists {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "input content path (argv[0]) does not exist.",
+        ));
+    }
+    if !input_is_dir {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "input content path is not a directory.",
+        ));
+    }
+
     Ok(argv)
 }
 
@@ -48,18 +81,15 @@ fn content_files_dir_entries(content_path: &Path) -> io::Result<Vec<DirEntry>> {
     Ok(dir_entries)
 }
 
-
-fn content_file_pathbufs(argv: Vec<String>) -> io::Result<Vec<PathBuf>> {
-    let content_path = &argv[1];
-    let content_path = Path::new(content_path);
-    if !content_path.exists() {
+fn content_file_pathbufs(input_content_path: PathBuf) -> io::Result<Vec<PathBuf>> {
+    if !input_content_path.exists() {
         return Err(io::Error::new(
             io::ErrorKind::NotFound,
             "path to content does not exist.",
         ));
     }
 
-    let content_file_dir_entries = content_files_dir_entries(&content_path)?;
+    let content_file_dir_entries = content_files_dir_entries(&input_content_path)?;
 
     let content_file_pathbufs = content_file_dir_entries
         .into_iter()
@@ -88,7 +118,6 @@ fn parse_content_meta_data_err(path: &str) -> io::Error {
     )
 }
 
-
 fn content_metadata(path: &PathBuf) -> Result<(NaiveDate, String), Box<dyn Error>> {
     let path = match path.file_name() {
         Some(path) => match path.to_str() {
@@ -110,10 +139,15 @@ fn content_metadata(path: &PathBuf) -> Result<(NaiveDate, String), Box<dyn Error
     Ok((date, name.to_string()))
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let argv = argv()?;
-    let content_file_paths = content_file_pathbufs(argv)?;
-
+fn files_maps(
+    content_file_paths: Vec<PathBuf>,
+) -> Result<
+    (
+        BTreeMap<PathBuf, (NaiveDate, String)>,
+        BTreeMap<i32, BTreeMap<u32, HashSet<u32>>>,
+    ),
+    Box<dyn Error>,
+> {
     let mut content_files_meta_data: BTreeMap<PathBuf, (NaiveDate, String)> = BTreeMap::new();
     let mut content_file_system_structure: BTreeMap<i32, BTreeMap<u32, HashSet<u32>>> =
         BTreeMap::new();
@@ -121,9 +155,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     for path_to_content_file in content_file_paths {
         let (date, name) = content_metadata(&path_to_content_file)?;
 
-        assert!(content_files_meta_data
+        if !content_files_meta_data
             .insert(path_to_content_file, (date, name))
-            .is_none());
+            .is_none()
+        {
+            return Err(Box::new(io::Error::new(
+                io::ErrorKind::Other,
+                "unexpected duplicate content file.",
+            )));
+        }
 
         let _ = content_file_system_structure
             .entry(date.year())
@@ -133,28 +173,77 @@ fn main() -> Result<(), Box<dyn Error>> {
             .insert(date.day());
     }
 
-    /* for path_to_content_file in content_file_paths {
-        let (date, name) = parse_content_metadata(&path_to_content_file)?;
-        let file_contents = fs::read_to_string(path_to_content_file)?;
+    Ok((content_files_meta_data, content_file_system_structure))
+}
 
-        let year_str = date.year().to_string();
-        let year_dir = Path::new(&year_str);
-        if !year_dir.exists() {
-            fs::create_dir(year_dir)?;
+fn friendly_filename(name: &str) -> String {
+    let mut result = Vec::with_capacity(name.len());
+    for c in name.chars() {
+        if c.is_alphanumeric() {
+            result.push(c.to_ascii_lowercase());
+        } else if c == ' ' {
+            result.push('_');
         }
+    }
+    result.iter().collect()
+}
 
+fn contents(title: String, content_file_path: &Path) -> io::Result<String> {
+    let contents = fs::read_to_string(content_file_path)?;
+    let contents = format!(
+        ":base-path: ../../../..
 
-    } */
-    dbg!(content_files_meta_data);
+include::{{base-path}}/head.adoc[]
 
-    for (year, months) in &content_file_system_structure {
+== {}
+
+{}",
+        title, contents
+    );
+
+    Ok(contents)
+}
+
+fn construct_filesystem_time_structure(
+    content_file_system_structure: BTreeMap<i32, BTreeMap<u32, HashSet<u32>>>,
+    output_content_path: &PathBuf,
+) -> io::Result<()> {
+    for (year, months) in content_file_system_structure {
         for (month, days) in months {
             for day in days {
                 let year_month_day = format!("{}/{:02}/{:02}", year, month, day);
-                dbg!(year_month_day);
+                let path_to_content_dir =
+                    format!("{}/{}", output_content_path.display(), year_month_day);
+                fs::create_dir_all(path_to_content_dir)?;
             }
         }
     }
+    Ok(())
+}
+
+fn construct_content_files(
+    content_files_meta_data: BTreeMap<PathBuf, (NaiveDate, String)>,
+    output_content_path: PathBuf,
+) -> io::Result<()> {
+    for (path, (date, name)) in content_files_meta_data {
+        let (year, month, day) = (date.year_ce().1, date.month(), date.day());
+        let year_month_day = format!("{}/{:02}/{:02}", year, month, day);
+        let path_to_content_dir = format!("{}/{}", output_content_path.display(), year_month_day);
+        let friendly_name = friendly_filename(&name);
+        let content_file_output_path = format!("{}/{}.adoc", path_to_content_dir, friendly_name);
+        let contents = contents(name, path.as_path())?;
+        fs::write(content_file_output_path, contents)?;
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let argv = argv()?;
+    let content_file_paths = content_file_pathbufs(argv.input_content_path)?;
+    let (content_files_meta_data, content_file_system_structure) = files_maps(content_file_paths)?;
+
+    construct_filesystem_time_structure(content_file_system_structure, &argv.output_content_path)?;
+    construct_content_files(content_files_meta_data, argv.output_content_path)?;
 
     Ok(())
 }
